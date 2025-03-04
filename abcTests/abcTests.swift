a93c325f646337bdc661250deb4c11b3b9a78a4b
OtogawaKatsutoshi <gh1ab7im@gmail.com>
2025-03-05 02:37:27 +0900

    Initial Commit

---
+++ abcTests/abcTests.swift
@@ -0,0 +1,17 @@
+//
+//  abcTests.swift
+//  abcTests
+//
+//  Created by Otogawa Katsutoshi on 2025/03/05.
+//
+
+import Testing
+@testable import abc
+
+struct abcTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
